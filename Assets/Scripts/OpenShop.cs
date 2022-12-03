db78f66dc996d728fe83bda76f1324c0d1a51486
Alejandro Jonela <ajonela46@gmail.com>
2022-12-04 02:40:28 +0800

    feat: open and close shop via keydown

--- Assets/Scripts/OpenShop.cs
+++ Assets/Scripts/OpenShop.cs
@@ -2,23 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using StarterAssets;
+using UnityEngine.InputSystem;
 
 public class OpenShop : MonoBehaviour
 {
     public GameObject Inventory;
-    private StarterAssetsInputs starterAssetsInputs;
+    //private StarterAssetsInputs starterAssetsInputs;
     // Start is called before the first frame update
     void Start()
     {
-        starterAssetsInputs = GetComponent<StarterAssetsInputs>();
+        // starterAssetsInputs = GetComponent<StarterAssetsInputs>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(starterAssetsInputs.openshop) {
+
+        if(!Inventory.activeSelf & Input.GetKeyDown(KeyCode.B)) 
+        {
+            Debug.Log("openshop");
             Inventory.SetActive(true);
+
         }
+        else if(Inventory.activeSelf & Input.GetKeyDown(KeyCode.B))
+        {
+            Inventory.SetActive(false);
+        }
+       
 
     }
 }
