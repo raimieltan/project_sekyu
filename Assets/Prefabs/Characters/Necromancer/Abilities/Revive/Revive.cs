b5dacaa61572bf047412f1352706e340e3448b8b
Veto Bastiero <vetobastiero@gmail.com>
2022-12-03 23:06:02 +0800

    fix: apply hud to mangkukulam + fix mangkukulam

--- Assets/Prefabs/Characters/Necromancer/Abilities/Revive/Revive.cs
+++ Assets/Prefabs/Characters/Necromancer/Abilities/Revive/Revive.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2950bde1d67af9e99b9f0ba0a39ed366bced6cc4bf101ac013afb2cfaedbc1ae
-size 3326
+oid sha256:aaf7fc42b2fdedd7a570aa3bddbbd523d72b1d0917a9c79d04f2fa1b094d7969
+size 3578
