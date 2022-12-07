c2a08144dfe1d7d563fa3e8404b5a2779f77d139
Veto Bastiero <vetobastiero@gmail.com>
2022-12-08 01:13:33 +0800

    feat: apply spawn traps and smoke

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73aa31c2c5d627325a6cc9215fec533419b502bc6f58c4c46f1a67a432791184
-size 3191
+oid sha256:05972134cab90805d55164120805bad691a3fc2e39bf095b6ae0177295aa2dcc
+size 2519
