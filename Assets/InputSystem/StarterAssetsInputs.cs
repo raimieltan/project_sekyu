1493305838e95e3a5286563c24071c1ef8e1eb65
Alejandro Jonela <ajonela46@gmail.com>
2022-12-08 21:55:48 +0800

    chore: disable lock cursor

--- Assets/InputSystem/StarterAssetsInputs.cs
+++ Assets/InputSystem/StarterAssetsInputs.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4c24d24691b25113fc431357400a69caaec0769870df3f67e8fc827b30f64c0
-size 4984
+oid sha256:9825b981b07d084d56a7e4dbd8b98b4a06e97e723c40793a3421fe50ed957846
+size 5011
