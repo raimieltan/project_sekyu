81e733011f77b7396f186ed679a625a86e15197a
Casey Rhyss Capacio <capaciocasey@gmail.com>
2022-12-10 21:51:36 +0800

    fix: bug on copycat

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a19c1eeaa1b40933d40b89c9d855e06125c14b1d7fa5fcf2b2643acaf4b7752
+oid sha256:d98f7b173f5dcab7f70e5728d23babea3a813378aae6fbb91dc6f58dbb931658
 size 3841
