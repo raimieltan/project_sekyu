b0d411f23f2de89bb7691a75a9ef3155c7bb1351
Alejandro Jonela <ajonela46@gmail.com>
2022-12-11 15:57:25 +0800

    feat: add openShop starterAssetsInput

--- Assets/InputSystem/StarterAssetsInputs.cs
+++ Assets/InputSystem/StarterAssetsInputs.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9825b981b07d084d56a7e4dbd8b98b4a06e97e723c40793a3421fe50ed957846
-size 5011
+oid sha256:99b49e91d802ee542758709acf814c09efe3c81405606fae51805b8f5fae0e69
+size 5272
