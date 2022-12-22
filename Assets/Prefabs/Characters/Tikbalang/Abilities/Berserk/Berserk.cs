a64930f9ad57c93eac892e3fc0c9bb2609bf81f4
Michael Handumon <kmcaelian@gmail.com>
2022-12-22 21:58:40 +0800

    chore: minor code cleanup

--- Assets/Prefabs/Characters/Tikbalang/Abilities/Berserk/Berserk.cs
+++ Assets/Prefabs/Characters/Tikbalang/Abilities/Berserk/Berserk.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20311ddac4df0dc3d98b6e5c5abcd5a2da97da72a57a139aff4ef85b36ae7072
-size 1580
+oid sha256:76800f5da0ffe3ee81442d9517de1d601caad8405ab9588e8eea43e3be6aeaa8
+size 1548
