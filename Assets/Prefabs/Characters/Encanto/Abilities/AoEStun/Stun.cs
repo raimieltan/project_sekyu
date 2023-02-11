b9402e4102e72de1cdfd9f832a1161237eb65893
Michael Handumon <kmcaelian@gmail.com>
2023-02-12 02:43:04 +0800

    feat: encanto stun ability

---
+++ Assets/Prefabs/Characters/Encanto/Abilities/AoEStun/Stun.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3a9af26e3c71415d0769e1b9eab7908e936a55fa7c1b6cab2a89fd45ab2cfde1
+size 3294
--- Assets/Prefabs/Characters/Encanto/Abilities/AoeHeal/Heal.cs
+++ Assets/Prefabs/Characters/Encanto/Abilities/AoeHeal/Heal.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:373850e78bb1767de6bb79b0b7caad99f898521a6a70d68ccd0187a421f2c4f7
-size 2249
+oid sha256:c13c10c25819c89975fd53ae8eb2773058e043721954414efb2c7d6226d15a07
+size 2609
