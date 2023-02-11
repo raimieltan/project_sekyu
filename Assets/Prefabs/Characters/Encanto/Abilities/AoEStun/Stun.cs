7e5899fc1a207daa5c56d4d7f5416c4d60532234
Michael Handumon <kmcaelian@gmail.com>
2023-02-12 03:24:21 +0800

    prefab changes

--- Assets/Prefabs/Characters/Encanto/Abilities/AoEStun/Stun.cs
+++ Assets/Prefabs/Characters/Encanto/Abilities/AoEStun/Stun.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a9af26e3c71415d0769e1b9eab7908e936a55fa7c1b6cab2a89fd45ab2cfde1
-size 3294
+oid sha256:3c5edc348969580c5f8ab8a739012f7346c0698ac230f989c9a39ae674f8c94b
+size 2966
