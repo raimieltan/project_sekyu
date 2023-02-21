135a68be45e9bab7a142e428e2a4d0472b51e76b
Michael Handumon <kmcaelian@gmail.com>
2023-02-21 12:47:42 +0800

    feat: changed Stun Aura prefab on Encanto Character to WhiteAura

--- Assets/Prefabs/Characters/Encanto/Abilities/AoEStun/Stun.cs
+++ Assets/Prefabs/Characters/Encanto/Abilities/AoEStun/Stun.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee547b4e9592c5d6f23177959a3172ba799aaa8669efdf614207139a54c97826
-size 2979
+oid sha256:abc7343de6b7345ca306b04880fb936a2bce895d34345c85dc7e868c2db38ffd
+size 2972
