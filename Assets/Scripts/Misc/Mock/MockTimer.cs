cd1a0761f2be80091e535ab44173ff406e7c8fff
Alejandro Jonela <58468148+Thevalk1@users.noreply.github.com>
2023-02-26 23:43:14 +0800

    Timer fix and Draw screen (#68)
    * feat: add Draw and Defeat screen
    
    * feat: connect Draw screen when timer is 0 and connect timer to photon

--- Assets/Scripts/Misc/Mock/MockTimer.cs
+++ Assets/Scripts/Misc/Mock/MockTimer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7593ab50e88467d09c6a9955e9af82ce4f2027ef491f20a07bba7882f0f3281d
-size 772
+oid sha256:c601f7f0f3ac8f722c54c91870c83a405004041ed596086e725cf47da6dfa89f
+size 3722
