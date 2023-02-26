83664ee649bd38611d98b5378c24326acde91913
Alejandro Jonela <ajonela46@gmail.com>
2023-02-26 19:18:01 +0800

    feat: add empty box item slot

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c94439758317f6c57e2601affaa67328bdf108d16976e874608a71d0e97c1f7d
-size 3091
+oid sha256:33b3b68d938a757a133807ac170b8df79690b38a30aa225c6eb09902cee26b9e
+size 4258
