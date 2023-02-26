a93adb2617ecbec78a19b6c8f9b8bcc856c2fdd1
Veto Bastiero <72717249+veto24@users.noreply.github.com>
2023-02-26 23:04:07 +0800

    Apply username input and store nickname in photon (#67)
    * feat: adds username input
    
    * fix: fix playerhud
    
    * feat: apply playername

--- Assets/Scripts/CreateAndJoinRooms.cs
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73e5dbe77957734c4c217bd52a7ad843d79c83da0ea1a75aa43d5bd5bd701c08
-size 1275
+oid sha256:27914116909876c487957c89a8fa591d903416ed791571394aa7dd6ad8bbd9c2
+size 1425
--- Assets/Scripts/DisplayPlayer.cs
+++ Assets/Scripts/DisplayPlayer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12f636b803371cfb93ced0c38605f5d442b55e2a6c05eeb48ab67d0179130504
-size 1870
+oid sha256:9c8a2f13f4d6cfe9efe62c7ebd1f5f7c370f694f0648abf06f379e0a06fca138
+size 1760
