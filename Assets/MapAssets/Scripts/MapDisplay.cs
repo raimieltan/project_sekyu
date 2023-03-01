a291037eeca0be94b281399190ace06529b96e0a
chaolen <siaotongcharlyn@gmail.com>
2023-03-01 10:05:16 +0800

    edit: update untracked files for room UI

--- Assets/MapAssets/Scripts/MapDisplay.cs
+++ Assets/MapAssets/Scripts/MapDisplay.cs
@@ -1,27 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MapDisplay : MonoBehaviour
-{
-    public Renderer textureRenderer;
-
-    public void DrawNoiseMap(float[,] noiseMap) {
-        int width = noiseMap.GetLength(0);
-        int height = noiseMap.GetLength(1);
-
-        Texture2D texture = new Texture2D(width, height);
-
-        Color[] colorMap = new Color[width * height];
-        for (int y=0; y < height; y++) {
-            for (int x=0; x < width; x++) {
-                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
-            }
-        }
-        texture.SetPixels(colorMap);
-        texture.Apply();
-
-        textureRenderer.sharedMaterial.mainTexture = texture;
-        textureRenderer.transform.localScale = new Vector3(width, 1, height);
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:460d05a78abc185a43338422bf26024b975b672ff43f2e171becde549ebf65ab
+size 827
--- Assets/MapAssets/Scripts/MapGenerator.cs
+++ Assets/MapAssets/Scripts/MapGenerator.cs
@@ -1,36 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MapGenerator : MonoBehaviour
-{
-    public int mapWidth;
-
-    public int mapHeight;
-
-    public float noiseScale;
-
-    // public int octaves;
-
-    // public float persistance;
-
-    // public float lacunarity;
-
-    public bool autoUpdate;
-
-    public void GenerateMap()
-    {
-        float[,] noiseMap =
-            Noise
-                .GenerateNoiseMap(mapWidth,
-                mapHeight,
-                noiseScale
-                // octaves,
-                // persistance,
-                // lacunarity
-                );
-
-        MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap (noiseMap);
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b77e110cb6bea15d2919b4f0209a07054326afe96f1d33e9dd28d5312052f917
+size 731
