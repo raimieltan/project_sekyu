80d5280afa6a61064ecd8f53a7cc727dd1eb3d10
Alejandro Jonela <58468148+Thevalk1@users.noreply.github.com>
2023-03-01 21:00:47 +0800

    Buying phase (#71)
    * feat: add buy phase script to shop
    
    * chore: update player prefab to show buyphase count down on screen
    
    * chore: update buyphase to 30 seconds

--- Assets/UI/Shop/General/Scripts/OpenShop.cs
+++ Assets/UI/Shop/General/Scripts/OpenShop.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b421e4591e8f855bdc2c9dc9a46ba6531bcd674c35add65fe912df327f5e9eb
-size 808
+oid sha256:5386c04ac0aa575b6686664bba25961b4a56ee92bf7680aa7ccc42c0dba30a7a
+size 1833
