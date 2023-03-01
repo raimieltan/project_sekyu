cc400af4322d2903a0804ac44a2473611d3090c5
Alejandro Jonela <ajonela46@gmail.com>
2023-03-01 14:15:11 +0800

    feat: add buy phase script to shop

--- Assets/UI/Shop/General/Scripts/OpenShop.cs
+++ Assets/UI/Shop/General/Scripts/OpenShop.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b421e4591e8f855bdc2c9dc9a46ba6531bcd674c35add65fe912df327f5e9eb
-size 808
+oid sha256:9f8e5b62abbc2e24969e4cb645a96daa2aabad91772a3220cc2f5803c2be108b
+size 2156
