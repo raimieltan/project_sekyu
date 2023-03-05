2928994d1f81bf400aea24c8bb5443c7cd405590
raimieltan <raimieltan@gmail.com>
2023-03-05 18:03:27 +0800

    chore: rebased main to current branch

--- Assets/Scripts/CreateAndJoinRooms.cs
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -1,3 +1,48 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:27914116909876c487957c89a8fa591d903416ed791571394aa7dd6ad8bbd9c2
-size 1425
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField nameInput;
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+
+    public void CreateRoom()
+    {
+        string mes = "Trying to create a room";
+        Debug.Log(mes);
+        int randomRoomName = Random.Range(0, 10000);
+        RoomOptions roompos = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 6 };
+        roompos.CustomRoomProperties = new ExitGames.Client.Photon.Hashtable();
+        roompos.CustomRoomProperties.Add("offsetX", Random.Range(0f, 9999f));
+        roompos.CustomRoomProperties.Add("offsetY", Random.Range(0f, 9999f));
+        PhotonNetwork.CreateRoom("Room" + randomRoomName.ToString(), roompos);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LocalPlayer.NickName = nameInput.text;
+        PhotonNetwork.LoadLevel("Room");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        string mes = "Create Room Failed";
+        Debug.Log(mes);
+        CreateRoom();
+    }
+}
