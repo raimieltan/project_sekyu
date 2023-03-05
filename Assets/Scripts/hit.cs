42ecb939f513fb447e6344c68699dbe763fb5318
Michael Handumon <kmcaelian@gmail.com>
2023-03-06 00:15:05 +0800

    feat: minor prefab changes

--- Assets/Scripts/hit.cs
+++ Assets/Scripts/hit.cs
@@ -1,3 +1,67 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:be56c962766b10557a0b736ad1e43baba0b5da58380cac8bafcb7897649bf70d
-size 1497
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class hit : MonoBehaviour
+{
+    public Health health;
+    public PhotonView view;
+    private ParticleSystem bloodAura;
+    public bool damageTaken;
+
+    void Awake(){
+        bloodAura = transform.Find("Geometry/BloodAura")?.GetComponent<ParticleSystem>();
+        if (bloodAura == null) {
+            Debug.LogError("Failed to find BloodAura particle system");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Weapon")
+        {
+            Damage damage = other.gameObject.GetComponent<Damage>();
+            StartCoroutine(EndDamageTaken());
+            applyDamage(damage.value);
+        }
+    }
+
+    private void applyDamage(float value)
+    {
+
+        Debug.Log(health.currentHealth);
+        // view.RPC("emitAuraBlood",RpcTarget.All);
+        health.TakeDamage(value);
+
+        if (health.currentHealth <= 0)
+        {
+            PhotonNetwork.Destroy(this.gameObject);
+        }
+
+	}
+
+    // [PunRPC]
+    // void emitAuraBlood() {
+
+    //         bloodAura.Play();
+    //         StartCoroutine(EndBlood());
+
+    // }
+
+    // IEnumerator EndBlood()
+    // {
+    //     yield return new WaitForSeconds(0.5f);
+    //     bloodAura.Stop();
+
+    // }
+
+    IEnumerator EndDamageTaken()
+    {
+        damageTaken = true;
+        yield return new WaitForSeconds(2.0f);
+        damageTaken = false;
+    }
+}
