647ffbbc7f11546e008860ca6b4c21e86bd1511e
Kyle Osunero <20hardrock12@gmail.com>
2023-03-05 20:49:12 +0800

    Flash bang prefab (#69)
    * feat: added flash bang prefab
    
    * fix: smoke particle system
    
    * feat: allow spawn of flashbang
    
    * refactor: instantiating flashbang
    
    ---------
    
    Co-authored-by: Veto Bastiero <vetobastiero@gmail.com>

--- Assets/Scripts/hit.cs
+++ Assets/Scripts/hit.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93f3f6959adc99228dca044d503b5dc52e69b293cba26bf1a52850a26938626c
-size 1381
+oid sha256:be56c962766b10557a0b736ad1e43baba0b5da58380cac8bafcb7897649bf70d
+size 1497
