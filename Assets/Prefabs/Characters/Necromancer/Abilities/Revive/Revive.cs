b4a1834e7b6d12207931b24d56586cbf49375acd
Kyle Osunero <20hardrock12@gmail.com>
2023-03-05 20:49:12 +0800

    Flash bang prefab (#69)
    * feat: added flash bang prefab
    
    * fix: smoke particle system
    
    * feat: allow spawn of flashbang
    
    * refactor: instantiating flashbang
    
    ---------
    
    Co-authored-by: Veto Bastiero <vetobastiero@gmail.com>

--- Assets/Prefabs/Characters/Necromancer/Abilities/Revive/Revive.cs
+++ Assets/Prefabs/Characters/Necromancer/Abilities/Revive/Revive.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e4a288601b066380fcd3bcf491c1556293ee5adc93da79ebe93551c3fcf843e
-size 3772
+oid sha256:463d8526f4b748369877b046d620b775f8ad86fb8f23b84e5ab00705e9014701
+size 3773
---
+++ Assets/Prefabs/Flashbang/Script/FlashBang.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2e0a385e1fa4486a40bfb38992c09ac5ff8c143fe4f21618abd5ecd4d0b7c14c
+size 5247
