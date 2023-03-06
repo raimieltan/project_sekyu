08830351f7b0b7611522d11de5cab06dd6f5b722
Michael Handumon <kmcaelian@gmail.com>
2023-03-06 11:24:06 +0800

    fix: encanto heal fix and nuno aura fix

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -69,14 +69,16 @@ public class Health : MonoBehaviour
 
     public void RestoreHealth(float healAmount)
     {
-        if (currentHealth + healAmount >= initialHealth)
-        {
-            currentHealth = initialHealth;
-        }
-        else
-        {
-            currentHealth += healAmount;
-        }
+       if (currentHealth <= initialHealth) {
+            if (currentHealth + healAmount >= initialHealth)
+            {
+                currentHealth = initialHealth;
+            }
+            else
+            {
+                currentHealth += healAmount;
+            }
+       }
 
         TriggerUpdateHealth(currentHealth);
     }
