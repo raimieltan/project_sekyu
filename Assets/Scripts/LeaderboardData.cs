5ec568425db73a280170e405b864d7c20bfc585c
caseyrhyss <capaciocasey@gmail.com>
2023-03-06 18:49:14 +0800

    feat: add leaderboard manager

---
+++ Assets/Scripts/LeaderboardData.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:df610a72d1473f2d7d9e025a8f9faeac04fe8027f90546b9b98b7e7950ee33b3
+size 302
---
+++ Assets/Scripts/LeaderboardManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1f519739ecd9b9049fb2a67518f43681bdd887dfb2b5715ca72e855f2f462a6c
+size 4148
