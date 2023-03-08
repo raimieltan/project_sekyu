5081821a2ffe30cb9784d78eed64cf14d88ff99e
caseyrhyss <capaciocasey@gmail.com>
2023-03-08 15:49:20 +0800

    feat: add scoreboard

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2c538e558b00d101f54c2d3bf38e492231362b177f4fedd8a2339515956c8813
+size 2640
---
+++ Assets/Scripts/ScoreboardCanvas.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2345d9b56ab8a2988f5a57082513ae49f121f20028045010c54e6fc2d54325e3
+size 594
