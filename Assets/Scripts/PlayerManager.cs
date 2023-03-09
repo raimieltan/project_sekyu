1d26d84b1afcedef7a373970a5c5e578573d0b71
caseyrhyss <capaciocasey@gmail.com>
2023-03-09 10:50:02 +0800

    chore: remove player manager

--- Assets/Scripts/PlayerManager.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:08f0c85b9c63538f8d3a75f0ccfd34faabdaff229cf4c291a8440d82552da963
-size 2253
--- Assets/Scripts/hit.cs
+++ Assets/Scripts/hit.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec2aea1d574ca0dc900ff99d7535d29d90bd23ebc054950a9caf580075f03002
-size 2898
+oid sha256:610a4fa50c072f21beede440bc2ae54633849512d8f13b26509455c31578b18c
+size 2540
