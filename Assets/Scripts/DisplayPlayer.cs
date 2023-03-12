e9a5f5df1242bad3ad0c0b9fe047b065805c3e95
raimieltan <raimieltan@gmail.com>
2023-03-12 22:31:05 +0800

    refactor: changed room flow to have room input

--- Assets/Scripts/DisplayPlayer.cs
+++ Assets/Scripts/DisplayPlayer.cs
@@ -5,40 +5,78 @@ using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
+using System.Linq;
 
 public class DisplayPlayer : MonoBehaviourPunCallbacks
 {
  
     public TextMeshProUGUI numberOfPlayers;
+    public TextMeshProUGUI tubigPlayers;
+    public TextMeshProUGUI dugoPlayers;
     public GameObject startButton;
     ExitGames.Client.Photon.Hashtable character = new ExitGames.Client.Photon.Hashtable();
 
     void Start () {
         numberOfPlayers.text = PhotonNetwork.PlayerList.Length + "/6";
-        Debug.Log(PhotonNetwork.LocalPlayer.NickName);
-        DontDestroyOnLoad(gameObject);
         PhotonNetwork.AutomaticallySyncScene = true;
-        Debug.Log("________Actor");
-        Debug.Log(PhotonNetwork.LocalPlayer.ActorNumber);
-        Debug.Log("________Room");
-        Debug.Log((int)PhotonNetwork.CurrentRoom.CustomProperties["room_creator"] * -1);
         if((int)PhotonNetwork.LocalPlayer.ActorNumber == (int)PhotonNetwork.CurrentRoom.CustomProperties["room_creator"] * -1) {
             startButton.SetActive(true);
         }
+        UpdatePlayerList();
     }
     void Update()
     {
         numberOfPlayers.text = PhotonNetwork.PlayerList.Length + "/6";
+        UpdatePlayerList();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        UpdatePlayerList();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdatePlayerList();
+    }
+
+    void UpdatePlayerList()
+    {
+        List<Player> players = PhotonNetwork.PlayerList.ToList();
+        tubigPlayers.text = "";
+        dugoPlayers.text = "";
+        foreach (Player player in players)
+        {
+            if(player.CustomProperties["team"] != null) {
+               
+                if((string)player.CustomProperties["team"] == "team1") {
+                 
+                    tubigPlayers.text += player.NickName + "\n";
+                } else if((string)player.CustomProperties["team"] == "team2") {
+                    dugoPlayers.text += player.NickName + "\n";
+                }
+                
+            }
+        
+        }
+        
     }
 
     public void ChooseTeam1() {
+        tubigPlayers.text = "";
+        dugoPlayers.text = "";
         character["team"] = "team1";
         PhotonNetwork.SetPlayerCustomProperties(character);
+        UpdatePlayerList();
     }
 
     public void ChooseTeam2() {
+        tubigPlayers.text = "";
+        dugoPlayers.text = "";
+        Debug.Log("Clicked");
         character["team"] = "team2";
         PhotonNetwork.SetPlayerCustomProperties(character);
+        UpdatePlayerList();
     }
 
 
