5e224a223f5e76a28d3d2e2a4536ac53ae1734ad
Aftertaste26 <20hardrock12@gmail.com>
2023-03-14 03:12:17 +0800

    style: updated room ui

--- Assets/Scripts/DisplayPlayer.cs
+++ Assets/Scripts/DisplayPlayer.cs
@@ -1,31 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+using System.Linq;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
-using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class DisplayPlayer : MonoBehaviourPunCallbacks
 {
- 
     public TextMeshProUGUI numberOfPlayers;
-    public TextMeshProUGUI tubigPlayers;
-    public TextMeshProUGUI dugoPlayers;
-    public GameObject errorPanel;
-    public GameObject startButton;
-    public bool forTest;
-    ExitGames.Client.Photon.Hashtable character = new ExitGames.Client.Photon.Hashtable();
 
-    void Start () {
+    public TextMeshProUGUI tubigPlayers;
+
+    public TextMeshProUGUI dugoPlayers;
+
+    public GameObject teamBanner;
+
+    public Material team1BannerMaterial;
+
+    public Material team2BannerMaterial;
+
+    public GameObject errorPanel;
+
+    public GameObject startButton;
+
+    private Renderer bannerRenderer;
+
+    public bool forTest;
+
+    ExitGames.Client.Photon.Hashtable
+        character = new ExitGames.Client.Photon.Hashtable();
+
+    void Start()
+    {
+        // Get the current renderer component of the game object
+        bannerRenderer = teamBanner.GetComponent<Renderer>();
+
         numberOfPlayers.text = PhotonNetwork.PlayerList.Length + "/6";
         PhotonNetwork.AutomaticallySyncScene = true;
-        if((int)PhotonNetwork.LocalPlayer.ActorNumber == (int)PhotonNetwork.CurrentRoom.CustomProperties["room_creator"] * -1) {
+        if (
+            (int) PhotonNetwork.LocalPlayer.ActorNumber ==
+            (int) PhotonNetwork.CurrentRoom.CustomProperties["room_creator"] *
+            -1
+        )
+        {
             startButton.SetActive(true);
         }
         UpdatePlayerList();
     }
+
     void Update()
     {
         numberOfPlayers.text = PhotonNetwork.PlayerList.Length + "/6";
@@ -49,105 +73,105 @@ public class DisplayPlayer : MonoBehaviourPunCallbacks
         dugoPlayers.text = "";
         foreach (Player player in players)
         {
-            if(player.CustomProperties["team"] != null) {
-               
-                if((string)player.CustomProperties["team"] == "team1") {
-                 
+            if (player.CustomProperties["team"] != null)
+            {
+                if ((string) player.CustomProperties["team"] == "team1")
+                {
                     tubigPlayers.text += player.NickName + "\n";
-                } else if((string)player.CustomProperties["team"] == "team2") {
+                }
+                else if ((string) player.CustomProperties["team"] == "team2")
+                {
                     dugoPlayers.text += player.NickName + "\n";
                 }
-                
             }
-        
         }
-        
     }
 
-    public void ChooseTeam1() {
+    public void ChooseTeam1()
+    {
         tubigPlayers.text = "";
         dugoPlayers.text = "";
         character["team"] = "team1";
-        PhotonNetwork.SetPlayerCustomProperties(character);
+        bannerRenderer.material = team1BannerMaterial;
+        PhotonNetwork.SetPlayerCustomProperties (character);
         UpdatePlayerList();
     }
 
-    public void ChooseTeam2() {
+    public void ChooseTeam2()
+    {
         tubigPlayers.text = "";
         dugoPlayers.text = "";
         character["team"] = "team2";
-        PhotonNetwork.SetPlayerCustomProperties(character);
+        bannerRenderer.material = team2BannerMaterial;
+        PhotonNetwork.SetPlayerCustomProperties (character);
         UpdatePlayerList();
     }
 
-
-
-    public void ChooseTikbalang() {
-        character["chosen"] = 0;
-        PhotonNetwork.SetPlayerCustomProperties(character);
-    }
-
-    public void ChooseMangkukulam() {
-        character["chosen"] = 1;
-      
-        PhotonNetwork.SetPlayerCustomProperties(character);
-     
-    }
-
-    public void ChooseNuno() {
-        character["chosen"] = 2;
-      
-        PhotonNetwork.SetPlayerCustomProperties(character);
-     
-    }
-
-    public void ChooseEncanto() {
-        character["chosen"] = 3;
-      
-        PhotonNetwork.SetPlayerCustomProperties(character);
-     
-    }
-
-public void StartGame()
-{
-    // Check if both teams have players
-    int team1Count = 0;
-    int team2Count = 0;
-    foreach (Player player in PhotonNetwork.PlayerList)
+    public void ChooseTikbalang()
     {
-        if (player.CustomProperties["team"] != null)
+        character["chosen"] = 0;
+        PhotonNetwork.SetPlayerCustomProperties (character);
+    }
+
+    public void ChooseMangkukulam()
+    {
+        character["chosen"] = 1;
+
+        PhotonNetwork.SetPlayerCustomProperties (character);
+    }
+
+    public void ChooseNuno()
+    {
+        character["chosen"] = 2;
+
+        PhotonNetwork.SetPlayerCustomProperties (character);
+    }
+
+    public void ChooseEncanto()
+    {
+        character["chosen"] = 3;
+
+        PhotonNetwork.SetPlayerCustomProperties (character);
+    }
+
+    public void StartGame()
+    {
+        // Check if both teams have players
+        int team1Count = 0;
+        int team2Count = 0;
+        foreach (Player player in PhotonNetwork.PlayerList)
         {
-            if ((string)player.CustomProperties["team"] == "team1")
+            if (player.CustomProperties["team"] != null)
             {
-                team1Count++;
-            }
-            else if ((string)player.CustomProperties["team"] == "team2")
-            {
-                team2Count++;
+                if ((string) player.CustomProperties["team"] == "team1")
+                {
+                    team1Count++;
+                }
+                else if ((string) player.CustomProperties["team"] == "team2")
+                {
+                    team2Count++;
+                }
             }
         }
+        if (!forTest && (team1Count == 0 || team2Count == 0))
+        {
+            errorPanel.SetActive(true);
+            StartCoroutine(HideErrorMessage());
+            return;
+        }
+
+        // Start the game
+        PhotonNetwork.SetPlayerCustomProperties (character);
+        ExitGames.Client.Photon.Hashtable currentProperties =
+            PhotonNetwork.CurrentRoom.CustomProperties;
+        currentProperties.Add("WinningTeamID", "");
+        PhotonNetwork.CurrentRoom.SetCustomProperties (currentProperties);
+        PhotonNetwork.LoadLevel("Game");
     }
-    if ( !forTest && (team1Count == 0 || team2Count == 0))
+
+    private IEnumerator HideErrorMessage()
     {
-        errorPanel.SetActive(true);
-        StartCoroutine(HideErrorMessage());
-        return;
+        yield return new WaitForSeconds(3f);
+        errorPanel.SetActive(false);
     }
-
-    // Start the game
-    PhotonNetwork.SetPlayerCustomProperties(character);
-    ExitGames.Client.Photon.Hashtable currentProperties = PhotonNetwork.CurrentRoom.CustomProperties;
-    currentProperties.Add("WinningTeamID", "");
-    PhotonNetwork.CurrentRoom.SetCustomProperties(currentProperties);
-    PhotonNetwork.LoadLevel("Game");
 }
-
-private IEnumerator HideErrorMessage()
-{
-    yield return new WaitForSeconds(3f);
-    errorPanel.SetActive(false);
-}
-
-
-}
-
