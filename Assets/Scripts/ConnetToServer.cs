a4196511a098b35a8be04c85c383ec4496181770
Michael Handumon <kmcaelian@gmail.com>
2023-03-14 09:24:48 +0800

    fix: merge changes

--- Assets/Scripts/ConnetToServer.cs
+++ Assets/Scripts/ConnetToServer.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,28 +21,4 @@ public class ConnetToServer : MonoBehaviourPunCallbacks
         SceneManager.LoadScene("Lobby");
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Photon.Pun;
-using UnityEngine.SceneManagement;
-
-public class ConnetToServer : MonoBehaviourPunCallbacks
-{
-    private void Start() {
-        PhotonNetwork.ConnectUsingSettings();
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("Connecting to master");
-        PhotonNetwork.JoinLobby();
-    }
-
-    
-    public override void OnJoinedLobby(){
-        SceneManager.LoadScene("Lobby");
-    }
-}
->>>>>>> cd138fd7313d94b6aef7dfe6e4aa75f427263a05
+                                                                                                                                                                            
