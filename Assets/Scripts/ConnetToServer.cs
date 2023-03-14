df2befdd599a0bffd929c19c2fbb079d46398700
caseyrhyss <52787443+caseyrhyss@users.noreply.github.com>
2023-03-14 21:51:09 +0800

    Fix scoreboard scene (#112)
    * fix: on display scoreboard
    
    * fix: fixed scoreboard scene
    
    * fix: hide scoreboard canvas in player prefabs
    
    * fix: fixed scoreboard on character prefabs

--- Assets/Scripts/ConnetToServer.cs
+++ Assets/Scripts/ConnetToServer.cs
@@ -16,9 +16,9 @@ public class ConnetToServer : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinLobby();
     }
 
-    
+
     public override void OnJoinedLobby(){
         SceneManager.LoadScene("Lobby");
     }
 }
-                                                                                                                                                                            
+
